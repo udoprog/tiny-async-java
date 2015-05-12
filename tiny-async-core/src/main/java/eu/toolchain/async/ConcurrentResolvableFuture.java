39099d7a808562c70142f759398f069f89c69bc0
John-John Tedro <johnjohn.tedro@gmail.com>
2015-05-12 03:15:42 +0200

    [core] refactor ConcurrentResolvableFuture to make testable

--- tiny-async-core/src/main/java/eu/toolchain/async/ConcurrentResolvableFuture.java
+++ tiny-async-core/src/main/java/eu/toolchain/async/ConcurrentResolvableFuture.java
@@ -35,6 +35,15 @@ import lombok.RequiredArgsConstructor;
  */
 // @formatter:on
 public class ConcurrentResolvableFuture<T> implements ResolvableFuture<T> {
+    // waiting for value.
+    public static final int RUNNING = 0x0;
+    public static final int RESULT_UPDATING = 0x1;
+
+    /* valid end state, are required to be greater than RESULT_UPDATING, otherwise {@link isStateReady(int)} will fail. */
+    public static final int RESOLVED = 0x10;
+    public static final int FAILED = 0x11;
+    public static final int CANCELLED = 0x12;
+
     /**
      * The maximum number of spins allowed when busy-waiting.
      */
@@ -42,7 +51,7 @@ public class ConcurrentResolvableFuture<T> implements ResolvableFuture<T> {
 
     private final Object $lock = new Object();
 
-    private Sync sync = new Sync();
+    private final S sync;
 
     private volatile ArrayList<CB<T>> callbacks = new ArrayList<CB<T>>();
 
@@ -62,15 +71,20 @@ public class ConcurrentResolvableFuture<T> implements ResolvableFuture<T> {
      * @param caller The caller implementation to use.
      */
     public ConcurrentResolvableFuture(final AsyncFramework async, final AsyncCaller caller) {
+        this(async, caller, new Sync());
+    }
+
+    protected ConcurrentResolvableFuture(final AsyncFramework async, final AsyncCaller caller, final S sync) {
         this.async = async;
         this.caller = caller;
+        this.sync = sync;
     }
 
     /* transition */
 
     @Override
     public boolean resolve(T result) {
-        if (!this.sync.complete(Sync.RESOLVED, result))
+        if (!this.sync.complete(RESOLVED, result))
             return false;
 
         final ArrayList<CB<T>> entries = takeAndClear();
@@ -83,7 +97,7 @@ public class ConcurrentResolvableFuture<T> implements ResolvableFuture<T> {
 
     @Override
     public boolean fail(Throwable cause) {
-        if (!sync.complete(Sync.FAILED, cause))
+        if (!sync.complete(FAILED, cause))
             return false;
 
         final ArrayList<CB<T>> entries = takeAndClear();
@@ -101,7 +115,7 @@ public class ConcurrentResolvableFuture<T> implements ResolvableFuture<T> {
 
     @Override
     public boolean cancel(boolean mayInterruptIfRunning) {
-        if (!sync.complete(Sync.CANCELLED, null))
+        if (!sync.complete(CANCELLED, null))
             return false;
 
         final ArrayList<CB<T>> entries = takeAndClear();
@@ -118,14 +132,14 @@ public class ConcurrentResolvableFuture<T> implements ResolvableFuture<T> {
     public AsyncFuture<T> bind(AsyncFuture<?> other) {
         int state = this.sync.state();
 
-        if (!Sync.isReady(state)) {
+        if (!isStateReady(state)) {
             if (add(new AsyncFutureCB(other)))
                 return this;
 
             state = sync.poll();
         }
 
-        if (state == Sync.CANCELLED)
+        if (state == CANCELLED)
             other.cancel();
 
         return this;
@@ -136,24 +150,24 @@ public class ConcurrentResolvableFuture<T> implements ResolvableFuture<T> {
     public AsyncFuture<T> on(final FutureDone<? super T> done) {
         int state = this.sync.state();
 
-        if (!Sync.isReady(state)) {
+        if (!isStateReady(state)) {
             if (add(new DoneCB(done)))
                 return this;
 
             state = sync.poll();
         }
 
-        if (state == Sync.RESOLVED) {
-            caller.resolveFutureDone(done, (T) this.sync.result);
+        if (state == RESOLVED) {
+            caller.resolveFutureDone(done, (T) this.sync.result(state));
             return this;
         }
 
-        if (state == Sync.FAILED) {
-            caller.failFutureDone(done, (Throwable) this.sync.result);
+        if (state == FAILED) {
+            caller.failFutureDone(done, (Throwable) this.sync.result(state));
             return this;
         }
 
-        if (state == Sync.CANCELLED) {
+        if (state == CANCELLED) {
             caller.cancelFutureDone(done);
             return this;
         }
@@ -171,14 +185,14 @@ public class ConcurrentResolvableFuture<T> implements ResolvableFuture<T> {
     public AsyncFuture<T> on(FutureCancelled cancelled) {
         int state = this.sync.state();
 
-        if (!Sync.isReady(state)) {
+        if (!isStateReady(state)) {
             if (add(new CancelledCB(cancelled)))
                 return this;
 
             state = sync.poll();
         }
 
-        if (state == Sync.CANCELLED)
+        if (state == CANCELLED)
             caller.runFutureCancelled(cancelled);
 
         return this;
@@ -188,7 +202,7 @@ public class ConcurrentResolvableFuture<T> implements ResolvableFuture<T> {
     public AsyncFuture<T> on(FutureFinished finishable) {
         int state = sync.state();
 
-        if (!Sync.isReady(state) && add(new FinishedCB(finishable)))
+        if (!isStateReady(state) && add(new FinishedCB(finishable)))
             return this;
 
         caller.runFutureFinished(finishable);
@@ -200,15 +214,15 @@ public class ConcurrentResolvableFuture<T> implements ResolvableFuture<T> {
     public AsyncFuture<T> on(FutureResolved<? super T> resolved) {
         int state = this.sync.state();
 
-        if (!Sync.isReady(state)) {
+        if (!isStateReady(state)) {
             if (add(new ResolvedCB(resolved)))
                 return this;
 
             state = sync.poll();
         }
 
-        if (state == Sync.RESOLVED)
-            caller.runFutureResolved(resolved, (T) this.sync.result);
+        if (state == RESOLVED)
+            caller.runFutureResolved(resolved, (T) this.sync.result(state));
 
         return this;
     }
@@ -217,15 +231,15 @@ public class ConcurrentResolvableFuture<T> implements ResolvableFuture<T> {
     public AsyncFuture<T> on(FutureFailed failed) {
         int state = this.sync.state();
 
-        if (!Sync.isReady(state)) {
+        if (!isStateReady(state)) {
             if (add(new FailedCB(failed)))
                 return this;
 
             state = sync.poll();
         }
 
-        if (state == Sync.FAILED)
-            caller.runFutureFailed(failed, (Throwable) this.sync.result);
+        if (state == FAILED)
+            caller.runFutureFailed(failed, (Throwable) this.sync.result(state));
 
         return this;
     }
@@ -234,12 +248,12 @@ public class ConcurrentResolvableFuture<T> implements ResolvableFuture<T> {
 
     @Override
     public boolean isDone() {
-        return Sync.isReady(sync.state());
+        return isStateReady(sync.state());
     }
 
     @Override
     public boolean isCancelled() {
-        return Sync.isCancelled(sync.state());
+        return sync.state() == CANCELLED;
     }
 
     /* get result */
@@ -248,7 +262,7 @@ public class ConcurrentResolvableFuture<T> implements ResolvableFuture<T> {
     public T get() throws InterruptedException, ExecutionException {
         final int state = sync.state();
 
-        if (Sync.isReady(state))
+        if (isStateReady(state))
             return checkState(state);
 
         sync.acquire();
@@ -259,7 +273,7 @@ public class ConcurrentResolvableFuture<T> implements ResolvableFuture<T> {
     public T getNow() throws ExecutionException {
         final int state = sync.state();
 
-        if (!Sync.isReady(state))
+        if (!isStateReady(state))
             throw new IllegalStateException("sync state is not ready");
 
         return checkState(state);
@@ -269,7 +283,7 @@ public class ConcurrentResolvableFuture<T> implements ResolvableFuture<T> {
     public T get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
         final int state = sync.state();
 
-        if (Sync.isReady(state))
+        if (isStateReady(state))
             return checkState(state);
 
         if (!sync.acquire(unit.toNanos(timeout)))
@@ -281,11 +295,11 @@ public class ConcurrentResolvableFuture<T> implements ResolvableFuture<T> {
     @SuppressWarnings("unchecked")
     private T checkState(int state) throws ExecutionException, CancellationException {
         switch (state) {
-        case Sync.FAILED:
-            throw new ExecutionException((Throwable) this.sync.result);
-        case Sync.RESOLVED:
-            return (T) this.sync.result;
-        case Sync.CANCELLED:
+        case FAILED:
+            throw new ExecutionException((Throwable) this.sync.result(state));
+        case RESOLVED:
+            return (T) this.sync.result(state);
+        case CANCELLED:
             throw new CancellationException();
         default:
             throw new IllegalStateException("illegal state: " + state);
@@ -299,20 +313,20 @@ public class ConcurrentResolvableFuture<T> implements ResolvableFuture<T> {
     public <C> AsyncFuture<C> transform(Transform<? super T, ? extends C> transform) {
         final int state = sync.state();
 
-        if (!Sync.isReady(state))
+        if (!isStateReady(state))
             return async.transform(this, transform);
 
         // shortcut
 
-        if (state == Sync.CANCELLED)
+        if (state == CANCELLED)
             return async.cancelled();
 
-        if (state == Sync.FAILED) {
-            final Throwable e = (Throwable) sync.result;
+        if (state == FAILED) {
+            final Throwable e = (Throwable) sync.result(state);
             return async.failed(e);
         }
 
-        final T result = (T) sync.result;
+        final T result = (T) sync.result(state);
         final C transformed;
 
         try {
@@ -329,20 +343,20 @@ public class ConcurrentResolvableFuture<T> implements ResolvableFuture<T> {
     public <C> AsyncFuture<C> transform(final LazyTransform<? super T, ? extends C> transform) {
         final int state = sync.state();
 
-        if (!Sync.isReady(state))
+        if (!isStateReady(state))
             return async.transform(this, transform);
 
         // shortcut
 
-        if (state == Sync.CANCELLED)
+        if (state == CANCELLED)
             return async.cancelled();
 
-        if (state == Sync.FAILED) {
-            final Throwable e = (Throwable) sync.result;
+        if (state == FAILED) {
+            final Throwable e = (Throwable) sync.result(state);
             return async.failed(e);
         }
 
-        final T result = (T) sync.result;
+        final T result = (T) sync.result(state);
 
         try {
             return (AsyncFuture<C>) transform.transform(result);
@@ -356,18 +370,18 @@ public class ConcurrentResolvableFuture<T> implements ResolvableFuture<T> {
     public AsyncFuture<T> error(Transform<Throwable, ? extends T> transform) {
         final int state = sync.state();
 
-        if (!Sync.isReady(state))
+        if (!isStateReady(state))
             return async.error(this, transform);
 
         // shortcut
 
-        if (state == Sync.CANCELLED)
+        if (state == CANCELLED)
             return async.cancelled();
 
-        if (state == Sync.RESOLVED)
-            return async.resolved((T) sync.result);
+        if (state == RESOLVED)
+            return async.resolved((T) sync.result(state));
 
-        final Throwable cause = (Throwable) sync.result;
+        final Throwable cause = (Throwable) sync.result(state);
 
         final T transformed;
 
@@ -385,18 +399,18 @@ public class ConcurrentResolvableFuture<T> implements ResolvableFuture<T> {
     public AsyncFuture<T> error(LazyTransform<Throwable, ? extends T> transform) {
         final int state = sync.state();
 
-        if (!Sync.isReady(state))
+        if (!isStateReady(state))
             return async.error(this, transform);
 
         // shortcut
 
-        if (state == Sync.CANCELLED)
+        if (state == CANCELLED)
             return async.cancelled();
 
-        if (state == Sync.RESOLVED)
-            return async.resolved((T) sync.result);
+        if (state == RESOLVED)
+            return async.resolved((T) sync.result(state));
 
-        final Throwable cause = (Throwable) sync.result;
+        final Throwable cause = (Throwable) sync.result(state);
 
         try {
             return (AsyncFuture<T>) transform.transform(cause);
@@ -405,20 +419,19 @@ public class ConcurrentResolvableFuture<T> implements ResolvableFuture<T> {
         }
     }
 
-    @SuppressWarnings("unchecked")
     @Override
     public AsyncFuture<T> cancelled(Transform<Void, ? extends T> transform) {
         final int state = sync.state();
 
-        if (!Sync.isReady(state))
+        if (!isStateReady(state))
             return async.cancelled(this, transform);
 
         // shortcut
 
-        if (state == Sync.FAILED)
+        if (state == FAILED)
             return this;
 
-        if (state == Sync.RESOLVED)
+        if (state == RESOLVED)
             return this;
 
         final T transformed;
@@ -437,15 +450,15 @@ public class ConcurrentResolvableFuture<T> implements ResolvableFuture<T> {
     public AsyncFuture<T> cancelled(LazyTransform<Void, ? extends T> transform) {
         final int state = sync.state();
 
-        if (!Sync.isReady(state))
+        if (!isStateReady(state))
             return async.cancelled(this, transform);
 
         // shortcut
 
-        if (state == Sync.FAILED)
+        if (state == FAILED)
             return this;
 
-        if (state == Sync.RESOLVED)
+        if (state == RESOLVED)
             return this;
 
         try {
@@ -609,16 +622,64 @@ public class ConcurrentResolvableFuture<T> implements ResolvableFuture<T> {
         }
     }
 
-    private static class Sync extends AbstractQueuedSynchronizer {
-        // waiting for value.
-        private static final int RUNNING = 0x0;
-        private static final int RESULT_UPDATING = 0x1;
+    public static boolean isStateReady(int state) {
+        return state > RESULT_UPDATING;
+    }
 
-        // various end states
-        private static final int RESOLVED = 0x10;
-        private static final int FAILED = 0x11;
-        private static final int CANCELLED = 0x12;
+    public static boolean isStateCancelled(int state) {
+        return state == CANCELLED;
+    }
 
+    protected interface S {
+        /**
+         * Get the current state.
+         *
+         * This must be an atomic operation.
+         */
+        public int state();
+
+        /**
+         * Poll for the current state, this is guaranteed to never return {@code RESULT_UPDATING}.
+         */
+        public int poll();
+
+        /**
+         * Acquire the shared lock on this synchronizer.
+         *
+         * Will block until the shared lock has been acquired.
+         */
+        public void acquire() throws InterruptedException;
+
+        /**
+         * Acquire the shared lock on this synchronizer.
+         *
+         * Will block for {@code nanos} nanoseconds until the shared lock has been acquired.
+         *
+         * @return {@code true} if the lock was acquired, {@code false} otherwise.
+         */
+        public boolean acquire(long nanos) throws InterruptedException;
+
+        /**
+         * Move the synchronizer state to the given end state.
+         *
+         * @param state The end state to move the synchronizer into.
+         * @param result The result to associate with the end state, if not {@code null} will cause the synchronizer to
+         *            go into an intermediate {@code RESULT_UPDATING} state that has to be accounted for. Use
+         *            {@link #poll()} to avoid this.
+         * @return {@code true} if the given transition is valid and happened, {@code false} otherwise}.
+         */
+        public boolean complete(int state, Object result);
+
+        /**
+         * Fetch the result of the synchronizer.
+         *
+         * @param state The state that you expect the synchronizer to currently be in.
+         * @return The associated result, or {@code null} if there are none (e.g. state is {@code CANCELLED}).
+         */
+        public Object result(int state);
+    }
+
+    private static class Sync extends AbstractQueuedSynchronizer implements S {
         private static final long serialVersionUID = -5044031197562766649L;
 
         private Object result;
@@ -641,7 +702,8 @@ public class ConcurrentResolvableFuture<T> implements ResolvableFuture<T> {
          * @param result Result to provide.
          * @return {@code true} if the result was successfully provided, {@code false} otherwise.
          */
-        private boolean complete(int state, Object result) {
+        @Override
+        public boolean complete(int state, Object result) {
             // short path: no result to provide.
             if (result == null) {
                 if (!compareAndSetState(RUNNING, state))
@@ -659,6 +721,7 @@ public class ConcurrentResolvableFuture<T> implements ResolvableFuture<T> {
             return true;
         }
 
+        @Override
         public boolean acquire(long nanos) throws InterruptedException {
             if (!tryAcquireSharedNanos(-1, nanos))
                 return false;
@@ -666,6 +729,7 @@ public class ConcurrentResolvableFuture<T> implements ResolvableFuture<T> {
             return true;
         }
 
+        @Override
         public void acquire() throws InterruptedException {
             acquireSharedInterruptibly(-1);
         }
@@ -673,6 +737,7 @@ public class ConcurrentResolvableFuture<T> implements ResolvableFuture<T> {
         /**
          * Fetch state without spinning, useful for just 'taking a look' without guaranteeing that a value has been set.
          */
+        @Override
         public int state() {
             return getState();
         }
@@ -680,6 +745,7 @@ public class ConcurrentResolvableFuture<T> implements ResolvableFuture<T> {
         /**
          * Take the current state, and assert that a result has been set, if applicable.
          */
+        @Override
         public int poll() {
             // spin if the current state is changing.
             int s;
@@ -695,12 +761,12 @@ public class ConcurrentResolvableFuture<T> implements ResolvableFuture<T> {
             return s;
         }
 
-        public static boolean isReady(int state) {
-            return state > RESULT_UPDATING;
-        }
+        @Override
+        public Object result(int state) {
+            if (!isStateReady(state))
+                throw new IllegalStateException("given state is not a valid end-state");
 
-        public static boolean isCancelled(int state) {
-            return state == CANCELLED;
+            return result;
         }
     }
 }
